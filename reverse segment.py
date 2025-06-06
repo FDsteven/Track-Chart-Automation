9cb5c99d59b01fbcf0057673650d869c652b7eaf
Steven Shi <fdstevensdw@gmail.com>
2025-06-05 19:33:00 -0500

    Added Reverse Crew Districts

--- reverse segment.py
+++ reverse segment.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import glob
+import os
 mile_to_meter = 1609.34
-Input_data_path = "Track Chart csvs/"
-grades = pd.read_csv(Input_data_path + "bnsf-grades-all.csv")
-grades["LINE_SEGMENT"] = grades["LINE_SEGMENT"].str.replace(',', '')
-grades["LINE_SEGMENT"] = grades["LINE_SEGMENT"].astype(float)
-line_segments = grades["LINE_SEGMENT"].unique()
-for line_segment in line_segments:
-    line_segment_name = str(line_segment)
-    input_file_name = "line segment " + line_segment_name + ".csv"
-    output_file_name = "line segment " + line_segment_name + " reversed.csv"
+Input_data_path = "Crew Districts/"
+csv_files = glob.glob(os.path.join(Input_data_path, '*.csv'))
+
+for csv_name in csv_files:
+    print("Loading: " + csv_name)
+    input_file_name = csv_name
+    output_file_name = "Reverse " + csv_name
     original = pd.read_csv(input_file_name)
     print(original)
     reverse_grade = np.zeros(len(original))
@@ -28,5 +28,5 @@ for line_segment in line_segments:
         if original.loc[i,"DIRECTION"] == "L":
             reverse_segment.loc[i,"DIRECTION"] = "R"
     reverse_segment = reverse_segment[::-1].reset_index(drop=True)
-    print(line_segment_name + " Done")
+    print(input_file_name + " Done")
     reverse_segment.to_csv(output_file_name)
